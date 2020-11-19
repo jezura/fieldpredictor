5af1cc93935782f0121beb7473c49a803bfbb969
Jaroslav Ježek <jezurajj@seznam.cz>
2020-11-19 18:48:31 +0100

    Another attempt

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['GET'])
+def home():
+    year = request.args['year']
+    return year
