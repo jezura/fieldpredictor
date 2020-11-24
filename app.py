dfe8dd2f6ee764a4a078276e52d915755c0d3b5a
Jaroslav Ježek <jezurajj@seznam.cz>
2020-11-24 12:05:53 +0100

    Trying to print array row.

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 
 @app.route('/prepare-data')
-def getAndPrepareData():
+def getAndPrepareData() -> int:
     dataset = pd.read_csv('data/data.csv',encoding= 'unicode_escape')
     X = dataset.iloc[:, 0:4].values
     Y = dataset.iloc[:, 5:21].values
@@ -22,8 +22,7 @@ def getAndPrepareData():
     X[:, 2] = lcvzdelani.fit_transform(X[:, 2])
     lcobor = LabelEncoder()
     X[:, 3] = lcobor.fit_transform(X[:, 3])
-    print(X[1].shape)
-    return 'OK'
+    return X[1][1]
 
 
 @app.route('/', methods=['GET'])
