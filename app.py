b315d446f9abdb6ebbe7391954b83e3967859ef5
Jaroslav Ježek <jezurajj@seznam.cz>
2020-11-24 11:12:45 +0100

    Reorganized methods

--- app.py
+++ app.py
@@ -9,28 +9,20 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.pipeline import Pipeline
 app = Flask(__name__)
 
-def getData():
+
+@app.route('/prepare-data')
+def getAndPrepareData():
     dataset = pd.read_csv('data/data.csv',encoding= 'unicode_escape')
     X = dataset.iloc[:, 0:4].values
     Y = dataset.iloc[:, 5:21].values
-    return 1
 
-def encodaData():
     lcpohlavi = LabelEncoder()
     X[:, 0] = lcpohlavi.fit_transform(X[:, 0])
-    X
     lcvzdelani = LabelEncoder()
     X[:, 2] = lcvzdelani.fit_transform(X[:, 2])
-    X
     lcobor = LabelEncoder()
     X[:, 3] = lcobor.fit_transform(X[:, 3])
-    X
     return 1
-
-@app.route('/prepare-data')
-def prepareData():
-    getData()
-    encodaData()
 
 
 @app.route('/', methods=['GET'])
