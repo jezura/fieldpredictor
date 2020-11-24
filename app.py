b04f3c11bf17a0ca4e2509320026b515be240ce9
Jaroslav Ježek <jezurajj@seznam.cz>
2020-11-24 10:55:02 +0100

    Label encoding

--- app.py
+++ app.py
@@ -7,12 +7,30 @@ import pandas as pd
 import keras
 app = Flask(__name__)
 
-@app.route('/get-data')
 def getData():
     dataset = pd.read_csv('data/data.csv',encoding= 'unicode_escape')
     X = dataset.iloc[:, 0:4].values
     Y = dataset.iloc[:, 5:21].values
-    return 'OK'
+    return 1
+
+def encodaData():
+    from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+    from sklearn.pipeline import Pipeline
+    lcpohlavi = LabelEncoder()
+    X[:, 0] = lcpohlavi.fit_transform(X[:, 0])
+    X
+    lcvzdelani = LabelEncoder()
+    X[:, 2] = lcvzdelani.fit_transform(X[:, 2])
+    X
+    lcobor = LabelEncoder()
+    X[:, 3] = lcobor.fit_transform(X[:, 3])
+    X
+    return 1
+
+@app.route('/prepare-data')
+def prepareData():
+    getData()
+    encodaData()
 
 
 @app.route('/', methods=['GET'])
