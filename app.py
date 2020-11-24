3a509eced0b3f1001ffb6f6abe96f72a5d80a534
Jaroslav Ježek <jezurajj@seznam.cz>
2020-11-24 10:16:37 +0100

    getData (dataset) method was added

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 
 from flask import Flask
 from flask import request
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import keras
 app = Flask(__name__)
+
+@app.route('data/get-data', methods=['GET'])
+def getData():
+    dataset = pd.read_csv('data.csv',encoding= 'unicode_escape')
+    X = dataset.iloc[:, 0:4].values
+    Y = dataset.iloc[:, 5:21].values
+    return X
+
 
 @app.route('/', methods=['GET'])
 def home():
