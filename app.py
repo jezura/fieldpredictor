6939f3313c59f6d84d248819301840296bc7926c
Jaroslav Ježek <jezurajj@seznam.cz>
2020-11-24 13:15:31 +0100

    Another new version with saved model from collab

--- app.py
+++ app.py
@@ -2,30 +2,24 @@
 from flask import Flask
 from flask import request
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import keras
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.pipeline import Pipeline
+from tensorflow.keras.models import load_model
+import joblib
+
+def return_prediction(model, scaler):
+    pred = model.predict(scaler.transform(np.array([[
+        0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 55
+    ]])))
+
+    return pred
+
 app = Flask(__name__)
 
-
-@app.route('/prepare-data')
-def getAndPrepareData() -> int:
-    dataset = pd.read_csv('data/data.csv',encoding= 'unicode_escape')
-    X = dataset.iloc[:, 0:4].values
-    Y = dataset.iloc[:, 5:21].values
-
-    lcpohlavi = LabelEncoder()
-    X[:, 0] = lcpohlavi.fit_transform(X[:, 0])
-    lcvzdelani = LabelEncoder()
-    X[:, 2] = lcvzdelani.fit_transform(X[:, 2])
-    lcobor = LabelEncoder()
-    X[:, 3] = lcobor.fit_transform(X[:, 3])
-    return 1
+# Loading the model
+my_model = load_model('/model/final_fieldpredictor_model.h5')
+my_scaler = joblib.load("/model/myScaler.pkl")
 
 
-@app.route('/', methods=['GET'])
-def home():
-    year = request.args['year']
-    return year
+@app.route('/prediciton')
+def prediction():
+    results = return_prediction(model=my_model, scaler=my_scaler)
+    return results
